77e177052f24bbd7de8b3829b189fc7396a30e77
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-09-02 09:19:17 +0100

    Fix CORS for local frontend

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const authRoutes = require("./routes/auth.routes");
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: [
+            process.env.FRONTEND_URL,
+            "http://127.0.0.1:5500"
+        ],
         credentials: true
     })
 );
